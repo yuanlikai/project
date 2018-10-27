61718d19a87007be6981b8ef7ddf6924404a9176
ylk <1091372693@qq.com>
2018-10-27 15:55:20 +0800

    1
    1

---
+++ voucher/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import 'babel-polyfill'
+
+import qs from 'qs'
+Vue.prototype.Qs=qs;
+import axios from 'axios'
+Vue.prototype.Axios=axios;
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
