95a5976cdf7b03556ae387a63b8757e74984799e
ylk <1091372693@qq.com>
2018-11-10 15:31:27 +0800

    1
    1

--- partner/src/components/partner/disk.vue
+++ partner/src/components/partner/disk.vue
@@ -1,77 +1,136 @@
 <template>
   <Content :style="{padding: '0 16px 16px'}">
     <Breadcrumb :style="{margin: '16px 0'}">
-      <BreadcrumbItem>合作伙伴</BreadcrumbItem>
+      <BreadcrumbItem>报备</BreadcrumbItem>
       <BreadcrumbItem>踩盘报备</BreadcrumbItem>
     </Breadcrumb>
     <Card>
-      <div style="height: 600px">
+      <div style="height: 570px">
         <Table :columns="columns1" :data="data1"></Table>
+        <Spin size="large" fix v-if="spinShow"></Spin>
       </div>
+
+      <!--分页-->
+      <PagIng :page="total" @cut="cut" ref="pag"/>
     </Card>
   </Content>
 </template>
 
 <script type="text/ecmascript-6">
+  import PagIng from '../public/paging'
+
   export default {
+    components: {
+      PagIng
+    },
     data() {
       return {
+        spinShow: false,
+        total: 1,
         columns1: [
           {
             title: 'id',
-            key: 'name'
+            align: 'center',
+            width: 80,
+            key: 'put_id'
           },
           {
             title: '楼盘名称',
-            key: 'address'
+            align: 'center',
+            key: 'hou_name'
           },
           {
             title: '报备时间',
-            key: 'address'
+            align: 'center',
+            key: 'put_seetime'
           },
           {
             title: '报备手机号',
-            key: 'address'
+            align: 'center',
+            key: 'user_mobile'
           },
           {
             title: '公司',
-            key: 'address'
+            align: 'center',
+            key: 'put_firm'
           },
           {
             title: '踩盘时间',
-            key: 'address'
+            align: 'center',
+            key: 'put_seetime'
           },
           {
             title: '踩盘人数',
-            key: 'address'
+            align: 'center',
+            width: 100,
+            key: 'put_num'
           },
           {
             title: '对接人/手机号',
-            key: 'address'
+            align: 'center',
+            render: (h, params) => {
+              return h('p', `${params.row.put_butt} / ${params.row.put_mobile}`)
+            }
           },
           {
             title: '来往方式',
-            key: 'address'
+            align: 'center',
+            key: 'put_status',
+            render: (h, params) => {
+              let sta;
+              params.row.put_status === 0 ? sta = '自驾' : sta = '班车';
+              return h('p', sta)
+            }
           },
           {
             title: '操作',
-            key: 'address'
+            align: 'center',
+            width: 80,
+            key: 'address',
+            render: (h, params) => {
+              return h('p', {
+                style: {
+                  color: '#B81314',
+                  cursor: 'pointer'
+                }
+              }, '删除')
+            }
           },
         ],
-        data1: [
-          {
-            name: 'John Brown',
-            age: 18,
-            address: 'New York No. 1 Lake Park',
-            date: '2016-10-03'
-          }
-        ]
+        data1: []
       }
     },
-    methods: {}
+    methods: {
+      //切换页码
+      cut(i) {
+        this.diskList(i)
+      },
+
+      //踩盘报备列表
+      diskList(i) {
+        let v = this;
+        v.spinShow = !v.spinShow;
+        v.Axios.post('/partner/par/put', v.Qs.stringify({
+          type: 0,
+          num: 11,
+          page: i
+        })).then(res => {
+          if (res.data.error === 0) {
+            v.total = res.data.data.nodes;
+            v.data1 = res.data.data.info
+          } else {
+            v.$Message.error(res.data.errMsg)
+          }
+          v.spinShow = !v.spinShow
+        })
+      }
+    },
+    mounted() {
+      this.diskList(1)
+    }
   }
 </script>
 
-<style lang="less">
+<style scoped lang="less">
 
 </style>
--- partner/src/components/partner/place.vue
+++ partner/src/components/partner/place.vue
@@ -6,53 +6,115 @@
     </Breadcrumb>
     <Card>
       <div style="height: 600px">
+        <Row style="margin-bottom:16px">
+          <Col span="12">
+          <Button type="primary" icon="md-add">
+            添加分销
+          </Button>
+          </Col>
+        </Row>
         <Table :columns="columns1" :data="data1"></Table>
+        <Spin size="large" fix v-if="spinShow"></Spin>
       </div>
+      <!--分页-->
+      <PagIng :page="total" @cut="cut" />
     </Card>
   </Content>
 </template>
 
 <script type="text/ecmascript-6">
+  import PagIng from '../public/paging'
   export default {
+    components:{
+      PagIng
+    },
     data() {
       return {
+        spinShow:false,
+        total:1,
         columns1: [
           {
             title: 'id',
-            key: 'name'
+            align:'center',
+            width:80,
+            key: 'user_id'
           },
           {
             title: '分销名',
-            key: 'address'
+            key: 'user_name'
           },
           {
             title: '联系人',
-            key: 'address'
+            key: 'user_nick'
+          },
+          {
+            title: '手机号',
+            key: 'user_mobile'
           },
           {
             title: '带看量（月）',
-            key: 'address'
+            key: 'look'
           },
           {
             title: '踩盘数（月）',
-            key: 'address'
+            key: 'disk'
           },
           {
             title: '操作',
-            key: 'address'
+            width:120,
+            align:'center',
+            key: 'address',
+            render:(h,params)=>{
+              return h('div',{
+                style:{
+                  cursor:'pointer'
+                }
+              },[
+                h('span',{
+                  style:{
+                    color:'#3590F1',
+                    marginRight:'10px',
+                  }
+                },'修改'),
+                h('span',{
+                  style:{
+                    color:'#F16646',
+                  }
+                },'删除')
+              ])
+            }
           },
         ],
-        data1: [
-          {
-            name: 'John Brown',
-            age: 18,
-            address: 'New York No. 1 Lake Park',
-            date: '2016-10-03'
-          }
-        ]
+        data1: []
       }
     },
-    methods: {}
+    methods: {
+      //切换页码
+      cut(i){
+        this.placeList(i)
+      },
+
+      //管理分销列表
+      placeList(i){
+        let v = this;
+        v.spinShow=!v.spinShow
+        v.Axios.post('/partner/par/index',v.Qs.stringify({
+          num:10,
+          page:i
+        })).then(res=>{
+          if (res.data.error === 0) {
+            v.total=res.data.data.nodes;
+            v.data1 = res.data.data.info
+          } else {
+            v.$Message.error(res.data.errMsg)
+          }
+          v.spinShow=!v.spinShow
+        })
+      }
+    },
+    mounted(){
+      this.placeList(1)
+    }
   }
 </script>
 
