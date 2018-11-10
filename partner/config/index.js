9fb94ac84ed29c72ce0667a134ad01cc580d7456
ylk <1091372693@qq.com>
2018-11-10 16:03:04 +0800

    1
    1

--- partner/config/index.js
+++ partner/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: 'https://gsf.oss-cn-beijing.aliyuncs.com/',
 
     /**
      * Source Maps
