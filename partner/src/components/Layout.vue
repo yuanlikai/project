f6876930d9a90428f7177f6860da850edcd9ecde
ylk <1091372693@qq.com>
2018-11-20 10:25:32 +0800

    1
    1

--- partner/src/components/Layout.vue
+++ partner/src/components/Layout.vue
@@ -111,11 +111,12 @@
             :on-success="handleSuccess"
             :format="['jpg','jpeg','png']"
             :max-size="2048"
+            :with-credentials="true"
             :on-format-error="handleFormatError"
             :on-exceeded-size="handleMaxSize"
             :before-upload="handleBeforeUpload"
             type="drag"
-            action="/publics/picture/imgouter"
+            action="/apis/publics/picture/imgouter"
             style="display: inline-block;width:58px;">
             <div style="width: 58px;height:30px;line-height: 30px;color: #000;">
               更换头像
