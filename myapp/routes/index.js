7b6d9775263c9bb9db94eee6e872ffe7e67446af
ylk <1091372693@qq.com>
2018-11-29 18:32:44 +0800

    1
    1

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,24 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1091372693',
+    database: 'world'
+});
+
+connection.connect()
+
 /* GET home page. */
 router.get('/index', function (req, res, next) {
-        res.json(
-            {
-                "status": 200,
-                "Message": "大家好，我是头冷",
-                "data": {
-                    "name": "BeJson",
-                    "url": "http://www.bejson.com",
-                    "page": 88,
-                    "isNonProfit": true,
-                    "address": {"street": "科技园路.", "city": "江苏苏州", "country": "中国"},
-                    "links": [{"name": "Google", "url": "http://www.google.com"}, {
-                        "name": "Baidu",
-                        "url": "http://www.baidu.com"
-                    }, {"name": "SoSo", "url": "http://www.SoSo.com"}]
-                }
-            });
+        connection.query('select * from city', function (err, rows, fields) {
+            res.json(rows);
+        })
     }
 );
 
